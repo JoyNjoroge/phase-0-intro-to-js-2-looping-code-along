8d38d2444141e53823e558150faaf382db1e5914
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-24 11:07:42 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(name){
+    const messages=[];
+    for(let i=0; i<name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+console.log(writeCards(names));
+
+// Logs each number when counting down, starting from the number provided
+function countDown(start){
+     for (let i = start; i >= 0; i--){
+        console.log(i);
+    }
+}
+countDown(4);
